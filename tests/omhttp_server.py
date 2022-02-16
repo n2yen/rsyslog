5dbfefab27d56e50610d8783b52f2fb7aa99da78
Nelson Yen <nyen@salesforce.com>
2022-02-16 10:48:20 -0800

    clean up omhttp_server

--- tests/omhttp_server.py
+++ tests/omhttp_server.py
@@ -56,35 +56,20 @@ class MyHandler(BaseHTTPRequestHandler):
         if metadata['userpwd']:
             if not self.validate_auth():
                 return
-        response_delay_secs = metadata['fail_with_delay_secs']
-
-        if metadata['delay_response_at'] != 0 and metadata['posts'] == metadata['delay_response_at']:
-            print("request '{0}' is being delayed for: {1} secs".format(metadata['posts'], metadata['delay_secs']))
-            #print "request '{0}' is being delayed for: {1} secs".format(metadata['posts'], metadata['delay_secs']))
-            time.sleep(metadata['delay_secs'])
-
-        if metadata['fail_at'] != -1 and metadata['posts'] > metadata['fail_at']:
-            if response_delay_secs:
-                print("sleeping for response_delay_secs: {1}".format(response_delay_secs))
-                time.sleep(response_delay_secs)
-            self.send_response(400)
-            self.end_headers()
-            self.wfile.write(b'BAD REQUEST')
-            return
 
         if metadata['fail_with_400_after'] != -1 and metadata['posts'] > metadata['fail_with_400_after']:
-            if response_delay_secs:
-                print("sleeping for response_delay_secs: {0}".format(response_delay_secs))
-                time.sleep(response_delay_secs)
+            if metadata['fail_with_delay_secs']:
+                print("sleeping for: {0}".format(metadata['fail_with_delay_secs']))
+                time.sleep(metadata['fail_with_delay_secs'])
             self.send_response(400)
             self.end_headers()
             self.wfile.write(b'BAD REQUEST')
             return
 
         if metadata['posts'] > 1 and metadata['fail_every'] != -1 and metadata['posts'] % metadata['fail_every'] == 0:
-            if response_delay_secs:
-                print("sleeping for response_delay_secs: {0}".format(response_delay_secs))
-                time.sleep(response_delay_secs)
+            if metadata['fail_with_delay_secs']:
+                print("sleeping for: {0}".format(metadata['fail_with_delay_secs']))
+                time.sleep(metadata['fail_with_delay_secs'])
             self.send_response(500)
             self.end_headers()
             self.wfile.write(b'INTERNAL ERROR')
@@ -149,21 +134,15 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--interface', action='store', type=str, default='localhost', help='port')
     parser.add_argument('--fail-after', action='store', type=int, default=0, help='start failing after n posts')
     parser.add_argument('--fail-every', action='store', type=int, default=-1, help='fail every n posts')
-    parser.add_argument('--fail-at', action='store', type=int, default=-1, help='fail at n posts')
     parser.add_argument('--fail-with-400-after', action='store', type=int, default=-1, help='fail with 400 after n posts')
     parser.add_argument('--fail-with-delay-secs', action='store', type=int, default=0, help='fail with n secs of delay')
-    parser.add_argument('--delay-response-at', action='store', type=int, default=0, help='delay response at n posts')
-    parser.add_argument('--delay-secs', action='store', type=int, default=2, help='number of secs during delay')
     parser.add_argument('--decompress', action='store_true', default=False, help='decompress posted data')
     parser.add_argument('--userpwd', action='store', default='', help='only accept this user:password combination')
     args = parser.parse_args()
     metadata['fail_after'] = args.fail_after
     metadata['fail_every'] = args.fail_every
-    metadata['fail_at'] = args.fail_at
     metadata['fail_with_400_after'] = args.fail_with_400_after
     metadata['fail_with_delay_secs'] = args.fail_with_delay_secs
-    metadata['delay_response_at'] = args.delay_response_at
-    metadata['delay_secs'] = args.delay_secs
     metadata['decompress'] = args.decompress
     metadata['userpwd'] = args.userpwd
     server = HTTPServer((args.interface, args.port), MyHandler)
